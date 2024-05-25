2521b83d6b77765d6c76cbb030bd3e0730c85656
s.nakamura <shotenba@gmail.com>
2024-05-25 16:36:33 +0900

    リファクタ

--- src/main/java/springchatapp/demo/model/resource/UserResourceFactory.java
+++ src/main/java/springchatapp/demo/model/resource/UserResourceFactory.java
@@ -1,17 +1,13 @@
 package springchatapp.demo.model.resource;
 
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.security.crypto.password.PasswordEncoder;
 import springchatapp.demo.model.entity.UserEntity;
 
 public class UserResourceFactory {
-  @Autowired
-  private static PasswordEncoder passwordEncoder;
 
   public static UserResource create(UserEntity userEntity) {
     return UserResource.builder()
         .uid(userEntity.getUid().getValue())
-        .password(passwordEncoder.encode(userEntity.getPassword().getValue()))
+        .password(userEntity.getPassword().getValue())
         .build();
   }
 }
